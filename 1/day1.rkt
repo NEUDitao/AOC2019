41e0863addcc1e86233ee07747340235f77e2e73
NEUDitao <li.edwa@husky.neu.edu>
2019-12-02 01:16:37 -0500

    first commit

---
+++ 1/day1.rkt
@@ -0,0 +1,64 @@
+#lang racket
+
+(require rackunit
+         rackunit/text-ui)
+
+(define INPUT1 (open-input-file "input1.txt"))
+
+
+;; mass->module: Number -> Number
+;; produces the fuel requirement for a module as described https://adventofcode.com/2019/day/1
+
+(module+ test
+  (test-case "mass->module test"
+             (check-equal? (mass->module 14) 2)
+             (check-equal? (mass->module 1969) 654)
+             (check-equal? (mass->module 100756) 33583)))
+
+(define (mass->module mass)
+  (- (floor (/ mass 3)) 2))
+
+
+
+;; input->mass/module-sum: InputPort -> Number
+;; reads every line from the input port, and sums up their mass->module
+
+(module+ test
+  (test-case "input->mass/module-sum"
+             (check-equal? (input->mass/module-sum INPUT1) 3367126)))
+
+(define (input->mass/module-sum ip)
+  (define MASS-ON-LINE (read ip))
+  (cond
+    [(eof-object? MASS-ON-LINE) 0]
+    [else (+ (mass->module MASS-ON-LINE) (input->mass/module-sum ip))]))
+
+
+
+;; input->mass/module-sum-with-fuel: InputPort -> Number
+;; reads every line from the input port, and sums up mass->module, accounting for the fuel required
+;; for fuel
+
+(module+ test
+  (test-case "input->mass/module-sum"))
+
+(define (input->mass/module-sum-with-fuel ip)
+  (define MASS-ON-LINE (read ip))
+  (cond
+    [(eof-object? MASS-ON-LINE) 0]
+    [else (+ (mass->module/acc MASS-ON-LINE) (input->mass/module-sum-with-fuel ip))]))
+
+;; mass->module/acc: Number -> Number
+;; calculates the mass->module, taking into account fuel for fuel
+
+(module+ test
+  (test-case "mass->module/acc"
+             (check-equal? (mass->module/acc 14) 2)
+             (check-equal? (mass->module/acc 1969) 966)
+             (check-equal? (mass->module/acc 100756) 50346)))
+
+(define (mass->module/acc mass)
+  (define FUEL-FROM-MASS (mass->module mass))
+  (cond
+    [(zero? FUEL-FROM-MASS) 0]
+    [else (+ FUEL-FROM-MASS (mass->module/acc FUEL-FROM-MASS))]))
