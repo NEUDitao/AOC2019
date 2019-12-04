57a59c46ecee5d6b6146da3b1a8f8ce3cc8d750f
NEUDitao <li.edwa@husky.neu.edu>
2019-12-04 12:20:44 -0500

    4

---
+++ 4/4.rkt
@@ -0,0 +1,52 @@
+#lang racket
+
+(define RANGE-LOWER 248345)
+(define RANGE-HIGHER 746315)
+
+;; all-digits-increasing-for-six-digit-num?: Number -> Boolean
+(define (all-digits-increasing-for-six-digit-num? n)
+
+  ;; all-digits-increasing?/a: Number -> Boolean
+  ;; ACCUMULATOR acc is the value of the last digit
+  ;; ACCUMULATOR idx is the index that you're at in the number
+  (define (all-digits-increasing?/a num acc idx)
+    (cond
+      [(= idx 0) #t]
+      [else (and (<= (modulo num 10) acc)
+                 (all-digits-increasing?/a (floor (/ num 10)) (modulo num 10) (sub1 idx)))]))
+
+  (all-digits-increasing?/a n 9 6))
+
+;; contains-double?: Number -> Boolean
+(define (contains-double? n)
+  (check-duplicates (string->list (number->string n))))
+
+;  (define (contains-double?/a num last)
+;    (cond
+;      [(= num 0) #f]
+;      [else (or (= last (modulo num 10))
+;                (contains-double?/a (floor (/ num 10)) (modulo num 10)))]))
+;
+;  (contains-double?/a n -1))
+
+
+;; find-digits-that-satisfy-mass-double: Number Number -> [List-of Number]
+(define (find-digits-that-satisfy-mass-double curr highest)
+  (cond
+    [(> curr highest) '()]
+    [else (if (and (all-digits-increasing-for-six-digit-num? curr) (contains-double? curr))
+              (cons curr (find-digits-that-satisfy-mass-double (add1 curr) highest))
+              (find-digits-that-satisfy-mass-double (add1 curr) highest))]))
+
+(length (find-digits-that-satisfy-mass-double RANGE-LOWER RANGE-HIGHER))
+
+
+;; no-sequence-longer-than-2: Number -> Boolean
+(define (contains-sequence-only-2 n)
+  (define NUMBER-AS-LIST (string->list (number->string n)))
+  (ormap (lambda (x) (= (count (lambda (y) (char=? y x)) NUMBER-AS-LIST) 2)) NUMBER-AS-LIST))
+
+
+    
+
+(length (filter contains-sequence-only-2 (find-digits-that-satisfy-mass-double RANGE-LOWER RANGE-HIGHER)))
